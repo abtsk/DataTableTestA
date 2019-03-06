11ce6148e4db5d6c6fd61ba5a7eb2e7ef8e4def4
abtsk <taisuke.abe.xb@renesas.com>
2019-03-07 06:58:09 +0900

    ソフトコミット

---
+++ DataGridViewMasterVB/DataGridViewMasterVB/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 1)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(793, 411)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(3, 413)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(275, 37)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.Text = "Button1"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnRefresh As Button
+End Class
---
+++ DataGridViewMasterVB/DataGridViewMasterVB/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    'データ保持
+    Private dtForm As DataTable
+
+    '列定義　キー値
+    Private Enum CN
+        LOT_NO
+        PROD_NM
+        KIND_NM
+        CARTON_QTY
+        MAX_CARTON_QTY
+    End Enum
+
+    '列定義　列名
+    Private ReadOnly COL_SET As New Dictionary(Of String, String) From {
+        {CN.LOT_NO, "ロットNo"},
+        {CN.PROD_NM, "品名"},
+        {CN.KIND_NM, "製品型名"},
+        {CN.CARTON_QTY, "カートン数量"},
+        {CN.MAX_CARTON_QTY, "MAXカートン数量"}
+    }
+
+    Private Structure COL_NAME
+        Public LOT_NO As String
+        Public PROD_NM As String
+        Public KIND_NM As String
+        Public CARTON_QTY As String
+        Public MAX_CARTON_QTY As String
+    End Structure
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Dim cn As COL_NAME
+        cn.LOT_NO = "BL11001"
+        cn.PROD_NM = "R5JADSADS"
+        cn.KIND_NM = "QALDE-282"
+        cn.CARTON_QTY = "500"
+        cn.MAX_CARTON_QTY = "2000"
+
+        insertData(cn)
+
+    End Sub
+
+    Private Sub initialTable()
+
+        '列追加
+        For Each col As String In COL_SET.Values
+            dtForm.Columns.Add(col, Type.GetType("System.String"))
+        Next
+
+    End Sub
+
+    'test
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        dtForm = New DataTable
+
+        initialTable()
+
+
+
+    End Sub
+
+    Private Sub insertData(ByVal col As COL_NAME)
+        Dim row As DataRow
+        row = dtForm.NewRow
+        row(CN.LOT_NO) = col.LOT_NO                     'ロットNo
+        row(CN.PROD_NM) = col.PROD_NM                   '品名
+        row(CN.KIND_NM) = col.KIND_NM                   '製品外形
+        row(CN.CARTON_QTY) = col.CARTON_QTY             'カートン数量
+        row(CN.MAX_CARTON_QTY) = col.MAX_CARTON_QTY     'MAXカートン数量
+        dtForm.Rows.Add(row)
+
+        DataGridView1.DataSource = dtForm
+    End Sub
+
+
+
+End Class
